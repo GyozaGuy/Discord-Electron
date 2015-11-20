8f525e5b8d6d82dffb497574928942bcbe16782d
Steven Stromberg <steven.stromberg@intel.com>
2015-11-20 16:07:02 -0700

    Updated with tray icon, is now single instance, minimizes to tray

--- main.js
+++ main.js
@@ -1,65 +1,71 @@
+const AppName = 'Discord';
+const height = 750;
+const width = 1200;
+
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
-const Menu = require('menu');
 const Tray = require('tray');
+const Menu = require('menu');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const AppIcon = __dirname + '/images/app.png';
 
 // Report crashes to our server.
 electron.crashReporter.start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
+var sysTray = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-// Application icon
-var appIcon = __dirname + '/images/discord.png';
+var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+  if (mainWindow) {
+    mainWindow.show();
+    mainWindow.focus();
+  }
+  return true;
+});
 
-// Prepare the system tray
-var sysTray = null;
+if (shouldQuit) {
+  app.quit();
+  return;
+}
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-  // Set screen size
-  var height = 750;
-  var width = 1200;
-
-  // Get screen size
   var electronScreen = electron.screen;
   var size = electronScreen.getPrimaryDisplay().workAreaSize;
 
-  // Create tray menu
-  sysTray = new Tray(appIcon);
+  sysTray = new Tray(AppIcon);
   var contextMenu = Menu.buildFromTemplate([
-    { label: 'Show' },
-    { label: 'Quit' }
+    { label: 'Show', click: function() { mainWindow.show(); mainWindow.focus(); } },
+    { label: 'Quit', click: function() { app.quit(); } }
   ]);
-  sysTray.setToolTip('Discord');
+  sysTray.setToolTip(AppName);
   sysTray.setContextMenu(contextMenu);
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({ icon: appIcon, width: width, height: height, x: Math.round(size['width'] / 2 - width / 2), y: Math.round(size['height'] / 2 - height / 2) });
-
-  // and load the index.html of the app.
+  mainWindow = new BrowserWindow({ icon: AppIcon, width: width, height: height, x: Math.round(size['width'] / 2 - width / 2), y: Math.round(size['height'] / 2 - height / 2) });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  // NOTE: For links not in a webview?
+  // mainWindow.webContents.on('new-window', function(e, url) {
+  //   e.preventDefault();
+  //   require('shell').openExternal(url);
+  // });
 
-  // Emitted when the window is closed.
+  // Open the DevTools
+  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.closeDevTools();
+  // TODO: Fix the above super awful hacky method to get the page to load
+  // NOTE: This seems to only be a problem with Skype so far
+
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
+  });
+
+  mainWindow.on('minimize', function() {
+    mainWindow.hide();
   });
 });
