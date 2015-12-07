6dad4746d59e6511db873ce4fd41148737306ee2
Steven Stromberg <gyozaguy@gmail.com>
2015-12-06 23:10:22 -0700

    Removed redundant code, rearranged a couple lines

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// Change these to customize the app
+const URL = 'https://discordapp.com/channels/@me';
 const HEIGHT = 750;
 const WIDTH = 1200;
 
+// Everything below this should be the same for most apps
 const ELECTRON = require('electron');
 const PATH = require('path');
 const MENU = ELECTRON.Menu;
@@ -33,7 +36,7 @@ function createMainWindow() {
     }
   });
 
-  WIN.loadURL('https://discordapp.com/channels/@me');
+  WIN.loadURL(URL);
 
   WIN.on('focus', e => {
     sysTray.setImage(APPICON);
@@ -50,9 +53,6 @@ function createMainWindow() {
 }
 
 function showAndCenter(win) {
-  if (!win.isFocused()) {
-    sysTray.setImage(APPICON);
-  }
   center(win);
   win.show();
   win.focus();
