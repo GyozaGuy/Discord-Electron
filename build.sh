69aaef87b784c46ca600f74e51787f0c06452cc2
Steven Stromberg <gyozaguy@gmail.com>
2016-05-08 15:03:59 -0600

    Updated build version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=Discord-Electron
-VERSION=0.37.3
+VERSION=0.37.8
 electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
